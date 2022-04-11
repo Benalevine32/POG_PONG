c4ce43950538b4af1f3982b2e383ee6e0fdd5846
ZachVandecar <99353525+ZachVandecar@users.noreply.github.com>
2022-04-10 22:32:03 -0400

    making auto paddle work
    no longer crashes !

--- Assets/Scripts/autopaddle.cs
+++ Assets/Scripts/autopaddle.cs
@@ -6,70 +6,72 @@ public class autopaddle : MonoBehaviour
 {
 
 
+    //ZEV Start
+
     //Set speed and screen limits for paddles
-    
+    public float paddleSpeed = 5f;
     public float screenLimit = 4f;
 
-    private Rigidbody2D auto;   // thisssisisisisTHISSIS
+    private Rigidbody2D auto;
 
 
-
-
-
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        auto = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-
+        var vel = auto.velocity;
 
         var pos = transform.position;
 
 
 
-        
-
-
-
-
-
-        if (pos.y == 4F)// auto moves up?
-        {
-
-                while (pos.y != -4f)
-                {
-
-                pos.y = pos.y - 0.1f;
-
-
-
-            }
-
-
-        }
-        else if (pos.y == -4f)
-        {
-            while (pos.y != 4f)
-            {
-
-                 pos.y = pos.y + 0.1f;
-
-    
-            }
-        }
        
-        
 
 
 
 
 
+
+
+        if (pos.y < 0)//Keeps paddle from traveling offscreen
+        {
+            vel.y = -paddleSpeed;
+
+
+        }
+        else if (pos.y == -screenLimit)
+        {
+            vel.y = paddleSpeed;
+        }
+
+        auto.velocity = vel;
+
+
+
+
+
+
+
+
+
+        /*
+        if (pos.y > screenLimit)//Keeps paddle from traveling offscreen
+        {
+            pos.y = screenLimit;
+        }
+        else if (pos.y < -screenLimit)
+        {
+            pos.y = -screenLimit;
+        }
+        transform.position = pos;
+
+        */
     }
-
-
+    //ZEV End
 }
